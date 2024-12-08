487660af7e7347e90baf82218de7a8c040a72978
Rion <pien@piennu777.jp>
2024-12-08 15:53:35 +0900

    Fix + Update

--- api/index.go
+++ api/index.go
@@ -1,10 +1,44 @@
 package handler
- 
+
 import (
-  "fmt"
-  "net/http"
+	"encoding/json"
+	"math/rand"
+	"net/http"
 )
- 
+
+type LotteryResult struct {
+	Prize string `json:"prize"`
+}
+
+var prizes = []string{
+	"大当たり",
+	"中当たり",
+	"小当たり",
+	"大外れ",
+	"中外れ",
+	"小外れ",
+	"帰れ",
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	result := LotteryResult{
+		Prize: prizes[rand.Intn(len(prizes))],
+	}
+
+	json.NewEncoder(w).Encode(result)
 }
